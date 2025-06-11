39576f710459b5d5fdde22eb01af21ebcf7c6dc0
Uzi <47419405+UziQidwai@users.noreply.github.com>
2025-06-11 15:47:49 -0400

    Update overwrites.ts

--- src/tokenlists/datadex/overwrites.ts
+++ src/tokenlists/datadex/overwrites.ts
@@ -17,7 +17,7 @@ export const overwrites: OverwritesForList = {
     }, //WVANA
     '0x202f120C83dCFcE04A1723aE7Ec7CdBd2eD73302': {
       logoURI:
-        'https://github.com/centfinance/tokenlists/blob/0b035c9136f4f5f7c3aa6f5cfb39dba46a42b556/src/assets/images/tokens/WWW.png',
+        'https://raw.githubusercontent.com/ykyrnow/ykyr-docs/refs/heads/main/public/images/www-logo-mark.svg',
     }, //WWWW
     '0xf23E379b2fd945F8c0A4F410Cb6EF9398bf022D6': {
       logoURI:
